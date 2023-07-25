8d2a9309d85a1546e00a792a5658fb078973f5ca
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-07-25 23:42:24 +0300

    Added scaling up the header image with doubleclick. Made all p tags white. Added click event to pop p tags up by clicking h2 - not working properly

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
+document.querySelector("img").addEventListener("dblclick", (e) => {
+  e.target.style.transform = "scale(1.3)";
+});
 
-// Kodlar buraya gelecek!
+const p = document.querySelectorAll("p");
+const h2 = document.querySelectorAll("h2");
+
+for (let i = 0; i < p.length; i++) {
+  p[i].style.color = "white";
+  h2[i].addEventListener("click", (e) => {
+    p[i].style.color = "black";
+  });
+}
